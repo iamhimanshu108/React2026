1aa9aa9ceb87834630bb04b682a30002c90c6902
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2026-09-02 08:39:39 +0000

    componets

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div classname='card'>Hello Himanshu
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, voluptatem.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
